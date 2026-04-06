9e5c43df191c23ce4f23b319ba952305e93b6d5d
teetunayak15-eng <teetunayak15@gmail.com>
2026-04-06 19:34:33 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,6 @@ module.exports = function(api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: [] // 🔥 remove reanimated plugin
+    plugins: []
   };
 };
